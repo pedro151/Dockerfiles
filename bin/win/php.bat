0f54ea1bab4ed83f9ce8c844176261c72ab2a073
pedro151 <phacl@hotmail.com>
2016-09-15 23:15:02 -0300

    up web

--- bin/win/php.bat
+++ bin/win/php.bat
@@ -2,4 +2,9 @@
 
 FOR /f "tokens=*" %%i IN ('docker-machine env default') DO %%i
 
-docker exec -it web php %*
+SET _ALLPARAM=%*
+SET _BARCONVERT=%_ALLPARAM:\=/%
+SET _PARAMS=%_BARCONVERT:D:=/Users%
+SET _PARAMS=%_BARCONVERT:C:=/c/Users%
+
+docker exec -it web php %_PARAMS%
--- bin/win/phpunit.bat
+++ bin/win/phpunit.bat
@@ -2,4 +2,10 @@
 
 FOR /f "tokens=*" %%i IN ('docker-machine env default') DO %%i
 
-docker exec -it web /bin/bash -c "cd /var/www; phpunit %*"
+SET _DIR=%cd%
+SET _ALLPARAM=%*
+SET _BARCONVERT=%_ALLPARAM:\=/%
+SET _PARAMS=%_BARCONVERT:D:=/Users%
+SET _PARAMS=%_BARCONVERT:C:=/c/Users%
+
+docker run -it --rm -v %_DIR%:%_DIR% -w %_DIR%  --net=host --sig-proxy=true --pid=host  originalbrownbear/php:7-cli-phpunit-xdebug phpunit %_PARAMS%
