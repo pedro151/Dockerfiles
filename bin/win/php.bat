8f027084010ad4fbf94618bd388deceeb2413e78
pedro151 <phacl@hotmail.com>
2016-09-23 10:48:41 -0300

    up web

--- bin/win/php.bat
+++ bin/win/php.bat
@@ -1,11 +1,34 @@
 @echo off
+setlocal ENABLEDELAYEDEXPANSION
 
 FOR /f "tokens=*" %%i IN ('docker-machine env default') DO %%i
 
-SET _DIR=%cd%
-SET _ALLPARAM=%*
-SET _BARCONVERT=%_ALLPARAM:\=/%
-SET _PARAMS=%_BARCONVERT:D:=/Users%
-SET _PARAMS=%_BARCONVERT:C:=/c/Users%
+Set _DIR=%cd%
+Set _PARAMS=%*
 
-docker exec -it web php %_PARAMS%
+Set "Proj=D:/htdocs/web"
+Set "ProjV=/var/www"
+
+Set "Driver_D=D:"
+Set "DriverV_D=/Users"
+
+Set "Driver_C=C:"
+Set "DriverV_C=/c"
+
+call :FUNC_REPLACE_DIR 
+call :FUNC_REPLACE_PARAM 
+ 
+docker exec web php %_PARAMS%
+echo:&goto:eof
+
+:FUNC_REPLACE_DIR
+Set _DIR=%_DIR:\=/%
+Set _DIR=!_DIR:%Proj%=%ProjV%!
+Set _DIR=!_DIR:%Driver_D%=%DriverV_D%!
+Set _DIR=!_DIR:%Driver_C%=%DriverV_C%!
+
+:FUNC_REPLACE_PARAM
+Set _PARAMS=%_PARAMS:\=/%
+Set _PARAMS=!_PARAMS:%Proj%=%ProjV%!
+Set _PARAMS=!_PARAMS:%Driver_D%=%DriverV_D%!
+Set _PARAMS=!_PARAMS:%Driver_C%=%DriverV_C%!
